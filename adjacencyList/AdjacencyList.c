d50ada9407e0013861ea6cb12dd26b7762e4251b
rui.alves284 <rui.alves284@live.com.pt>
2018-03-14 00:56:30 +0000

    Made a few changes to help read the code

--- adjacencyList/AdjacencyList.c
+++ adjacencyList/AdjacencyList.c
@@ -3,40 +3,52 @@
 
 typedef struct node{
 	int vertex;
-	struct node* next;
-}Node;
-
+	struct node *next;
+} Node;
 
 void Print(Node *list, int numberOfVertices);
-void InsertArc(Node *list, int a, int b); 
+void InsertArc(Node *list, int or, int dst);
 
 int main(){
-  int a,b;
+  int or,dst;
   int numberOfVertices;
   int numberOfArcs;
   int i;
-  
+
   scanf("%d",&numberOfVertices);
   Node list[numberOfVertices];
 
-  //Inicialização da lista. 
+  //Inicialização da lista.
   for(i=0; i<numberOfVertices; i++){
     list[i].vertex = i+1;
     list[i].next = NULL;
   }
-   
+
   //Leitura do numero de arestas
-  scanf("%d", &numberOfArcs);     
-        
+  scanf("%d", &numberOfArcs);
+
   //leitura dos vertices (a,b) - passagem para a função que insere um arco.
   for(i=0; i<numberOfArcs; i++){
-	scanf("%d %d", &a, &b);
-    InsertArc(list,a,b);
+	scanf("%d %d", &or, &dst);
+    InsertArc(list,or,dst);
   }
 
   Print(list, numberOfVertices);
 }
 
+void InsertArc(Node *list, int or, int dst){
+  Node *aux = (Node*) malloc(sizeof(Node));    //retorno de um ponteiro genérico.
+  aux->vertex = dst;
+  aux->next = NULL;
+
+  if(list[or-1].next == NULL) // Caso a list estiver vazia - Insere
+    list[or-1].next = aux;
+  else	{ // insere como primeiro da list. O(1)
+    Node *tmp = list[or-1].next;
+    aux->next = tmp;
+    list[or-1].next = aux;
+  }
+}
 
 void Print(Node *list, int numberOfVertices){
   int i;
@@ -52,34 +64,16 @@ void Print(Node *list, int numberOfVertices){
   }
 }
 
-void InsertArc(Node *list, int a, int b){
-  Node *aux;
-  Node *tmp;
- 
-  aux = (Node*) malloc((int)sizeof(Node));    //retorno de um ponteiro genérico.
-  aux->vertex = b;
-  aux->next = NULL;
-  
-  if(list[a-1].next == NULL) // Caso a list estiver vazia - Insere
-    list[a-1].next = aux;
-  else	{ // insere como primeiro da list. O(1) 
-    tmp = list[a-1].next;
-    aux->next = tmp;
-    list[a-1].next = aux;
-  }
-}
-
-
 //Para inserir ordenadamente:
 /* 		          //insere os vértices ordenados
-    else if (tmp->next == NULL) { 
+    else if (tmp->next == NULL) {
       aux->next = tmp->next;
-      tmp->next = aux;  
+      tmp->next = aux;
     }
     else {
       while((tmp->next != NULL) &&(tmp->next->vertex < b))
 	tmp = tmp->next;
       aux->next = tmp->next;
-      tmp->next = aux;  
+      tmp->next = aux;
     }*/
 //e adicionar um if(tmp->vertex > b) antes de aux->next = temp (l68~)
