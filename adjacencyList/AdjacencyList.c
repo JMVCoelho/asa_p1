db52517ead042177a889b73bbab774b50cb9669f
Rui Alves <rui.alves@marionete.co.uk>
2018-03-17 21:46:41 +0000

    tarjan working, aux data structures ready

--- adjacencyList/AdjacencyList.c
+++ adjacencyList/AdjacencyList.c
@@ -6,19 +6,25 @@ typedef struct node{
 	struct node *next;
 } Node;
 
-
 typedef struct stack{
 	int top;
-	int* vertices;
-}Stack;
+	int *vertices;
+} Stack;
 
+typedef struct scc{
+	int size;
+	int min;
+	int *vertices;
+	struct scc *next;
+} SCC;
 
 Stack vertices_stack;
-int numberOfSCCs;
+int numberOfSCCs = 0;
+SCC *listSCCs = NULL;
 
 void Print(Node *list, int numberOfVertices);
 void InsertArc(Node *list, int or, int dst);
-
+void freeList(Node *list, int numberOfVertices);
 
 void stack_push(int v, int numberOfVertices){
 	vertices_stack.top++;
@@ -44,27 +50,24 @@ int inStack(int vertex){
 	}
 	return 0;
 }
-	
+
 int stack_pop(){
 	return vertices_stack.top < 0 ? -1 : vertices_stack.vertices[vertices_stack.top--];
-}	
+}
 
-void TarjanVisit(int vertex,int numberOfVertices,  int* d, int* low, int* visited, Node* list){
-	
+void TarjanVisit(int vertex, int numberOfVertices, int *d, int *low, int *visited, Node *list, int *vertexBelongsToSCC, int *minSCC){
 	Node *tmp;
-	
 	d[vertex] = *visited;
-	low[vertex] = *visited;	
-	
-	
+	low[vertex] = *visited;
+
 	*visited=*visited+1;
 	stack_push(vertex, numberOfVertices);
-	
+
 	tmp = list[vertex].next;
 	while(tmp != NULL){
 		if(d[(tmp->vertex)-1]==-1 || inStack((tmp->vertex)-1)){
 			if(d[(tmp->vertex)-1]==-1){
-				TarjanVisit((tmp->vertex)-1, numberOfVertices, d, low, visited, list);
+				TarjanVisit((tmp->vertex)-1, numberOfVertices, d, low, visited, list, vertexBelongsToSCC, minSCC);
 			}
 			if(low[(tmp->vertex)-1]<low[vertex]){
 				low[vertex]=low[(tmp->vertex)-1];
@@ -74,56 +77,76 @@ void TarjanVisit(int vertex,int numberOfVertices,  int* d, int* low, int* visite
 	}
 	if(d[vertex]==low[vertex]){
 		int v;
-		//printf("SSC:\n");
-		numberOfSCCs++;
-		while((v =stack_pop()) != -1){
-			//printf("%d\n", v+1);
-			if(vertex == v){		
-				//printf("NEW SCC\n");
+		int min = 0;
+
+		while( (v = stack_pop()) != -1){
+			if (min == 0 || v < min) {
+				min = v;
+			}
+			vertexBelongsToSCC[v] = numberOfSCCs;
+			if(vertex == v){
+				//this SCC ends here
 				break;
 			}
-		}			
+		}
+		minSCC[numberOfSCCs] = min+1;
+		numberOfSCCs++;
 	}
-}	
-	
-	
+}
 
-
-int SCCTarjan(Node* list, int numberOfVertices){ 
+int SCCTarjan(Node *list, int numberOfVertices){
 	int i;
 	int visited = 0;
-	int* ptr_visited = &visited;
+	int *ptr_visited = &visited;
 
-	int d[numberOfVertices];
-	int low[numberOfVertices];
-	
+	int *d = (int*) malloc(sizeof(int)*numberOfVertices);
+	int *low = (int*) malloc(sizeof(int)*numberOfVertices);
+
+	int *vertexBelongsToSCC = (int*) malloc(sizeof(int)*numberOfVertices);
+	int *minSCC = (int*) malloc(sizeof(int)*numberOfVertices);
+
 	vertices_stack.vertices = (int*)malloc(sizeof(int)*numberOfVertices);
 	vertices_stack.top = -1;
-	
-	
+
 	for(i=0; i<numberOfVertices; i++){
 		d[i] = -1;
+		vertexBelongsToSCC[i] = 0;
+		minSCC[i] = 0;
 	}
-	
-	
+
 	for(i=0; i<numberOfVertices; i++){
 		if (d[i] == -1){
-			TarjanVisit(i,numberOfVertices, d, low, ptr_visited, list);
+			TarjanVisit(i, numberOfVertices, d, low, ptr_visited, list, vertexBelongsToSCC, minSCC);
 		}
 	}
-	
+
 	printf("SSCs:%d\n ", numberOfSCCs);
+	printf("belongs: ");
+	for(i=0; i<numberOfVertices; i++){
+		printf("%d  ", vertexBelongsToSCC[i]+1);
+	}
+	printf("\nmins: ");
+	for(i=0; i<numberOfVertices; i++){
+		printf("%d  ", minSCC[i]);
+	}
+
+
+	free(vertexBelongsToSCC);
+	free(minSCC);
+	free(d);
+	free(low);
 	return 0;
 }
 
 int main(){
   int or,dst;
   int numberOfVertices;
-  int numberOfArcs;
+  int numberOfEdges;
   int i;
 
   scanf("%d",&numberOfVertices);
-  Node list[numberOfVertices];
+  //Node list[numberOfVertices];
+	Node *list = (Node*)malloc(sizeof(Node)*numberOfVertices);
 
   //Inicialização da lista.
   for(i=0; i<numberOfVertices; i++){
@@ -132,28 +155,27 @@ int main(){
   }
 
   //Leitura do numero de arestas
-  scanf("%d", &numberOfArcs);
+  scanf("%d", &numberOfEdges);
 
   //leitura dos vertices (a,b) - passagem para a função que insere um arco.
-  for(i=0; i<numberOfArcs; i++){
+  for(i=0; i<numberOfEdges; i++){
 	scanf("%d %d", &or, &dst);
     InsertArc(list,or,dst);
   }
 	SCCTarjan(list, numberOfVertices);
+	freeList(list, numberOfVertices);
 }
 
 void InsertArc(Node *list, int or, int dst){
   Node *aux = (Node*) malloc(sizeof(Node));    //retorno de um ponteiro genérico.
   aux->vertex = dst;
-  aux->next = NULL;
 
   if(list[or-1].next == NULL) // Caso a list estiver vazia - Insere
-    list[or-1].next = aux;
+		aux->next = NULL;
   else	{ // insere como primeiro da list. O(1)
-    Node *tmp = list[or-1].next;
-    aux->next = tmp;
-    list[or-1].next = aux;
+    aux->next = list[or-1].next;
   }
+	list[or-1].next = aux;
 }
 
 void Print(Node *list, int numberOfVertices){
@@ -168,6 +190,18 @@ void Print(Node *list, int numberOfVertices){
     }
     printf("\n");
   }
+}
+
+void freeList(Node *list, int numberOfVertices) {
+	for (int i=0; i<numberOfVertices; i++) {
+		Node *head = &list[i];
+		while (head->next != NULL) {
+			Node *tmp = head->next;
+			head->next = tmp->next;
+			free(tmp);
+		}
+	}
+	free(list);
 }
 
 //Para inserir ordenadamente:
